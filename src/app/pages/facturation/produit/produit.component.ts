1b42ad380fab2d846a6d89367bb1a25339999b98
Bertrand KOUAM <bertrand.kouam.ext@socomar-cameroun.com>
2022-06-13 12:54:28 +0100

    add and list produit Ok

--- src/app/pages/facturation/produit/produit.component.ts
+++ src/app/pages/facturation/produit/produit.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { latLng, tileLayer } from 'leaflet';
+import { customersData } from 'src/app/pages/ecommerce/customers/data';
+import { Customers } from 'src/app/pages/ecommerce/customers/customers.model';
 
 @Component({
   selector: 'app-produit',
@@ -11,20 +14,78 @@ import { latLng, tileLayer } from 'leaflet';
 export class ProduitComponent implements OnInit {
 
   formData: FormGroup;
+  content:any;
+  customersData: Customers[];
+validationform: FormGroup;
 
-  constructor(public formBuilder: FormBuilder) {
-  }
+constructor(private modalService: NgbModal, public formBuilder: FormBuilder) { }
 
+submitted: boolean;
   
   // bread crumb items
   breadCrumbItems: Array<{}>;
 
-  ngOnInit(): void {
-    this.breadCrumbItems = [{ label: 'Produit' }, { label: 'Produit', active: true }];
-    this.formData = this.formBuilder.group({
-      message: ['', [Validators.required]],
+  
+ngOnInit(): void {
+  this.breadCrumbItems = [{ label: 'Produits' }, { label: 'Liste des produits', active: true }];
+
+  this.validationform = this.formBuilder.group({
+    name: ['', [Validators.required]],
+    phone: ['', [Validators.required]],
+    balance: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+    date: ['', [Validators.required]],
+  });
+
+  this._fetchData();
+}
+
+private _fetchData() {
+  this.customersData = customersData;
+}
+
+/**
+ * Returns form
+ */
+get form() {
+  return this.validationform.controls;
+}
+/**
+ * Modal Open
+ * @param content modal content
+ */
+openModal(content: any) {
+  this.modalService.open(content, { centered: true });
+}
+
+/**
+ * save the contacts data
+ */
+saveData() {
+  const name = this.validationform.get('name').value;
+  const phone = this.validationform.get('phone').value;
+  const balance = this.validationform.get('balance').value;
+  const email = this.validationform.get('email').value;
+  const date = this.validationform.get('date').value;
+  if (this.validationform.valid) {
+    this.customersData.push({
+      name,
+      phone,
+      balance,
+      email,
+      date
     });
-    //this._fetchData();
+    this.validationform = this.formBuilder.group({
+      name: '',
+      phone: '',
+      balance: '',
+      email: '',
+      date: ''
+    });
+    this.modalService.dismissAll();
   }
+  this.submitted = true;
+}
+
 
 }
